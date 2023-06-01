038d5c870a338c320ade2179a6c28a55d4bdea67
rm86061 <rm86061@BF-P4L1-M15.fieb.local>
2023-06-01 09:43:27 -0300

    Inicio Controle de Versão

---
+++ src/main/java/com/itb/inf2bm/comercio/moldel/Produto.java
@@ -0,0 +1,14 @@
+package com.itb.inf2bm.comercio.moldel;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: Acesso liberado para todas as calsses.
+	// private: Acesso liberado apenas para os membros da própia classe
+	
+}
